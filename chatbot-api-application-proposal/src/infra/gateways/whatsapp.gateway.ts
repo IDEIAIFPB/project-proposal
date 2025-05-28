6a8a8d962f2572157db96cd716f86041b1229833
Caio Vinicius <caio.araujo@aquare.la>
2025-05-28 02:38:48 -0300

    Feature: Implements logs save

--- chatbot-api-application-proposal/src/infra/gateways/whatsapp.gateway.ts
+++ chatbot-api-application-proposal/src/infra/gateways/whatsapp.gateway.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { Env } from '../../config/env.config';
-import { WhatsAppGatewayInterface } from 'src/domain/interfaces/whatsapp-gateway.interface';
-
+import { WhatsAppGatewayInterface } from '@src/domain/interfaces/whatsapp-gateway.interface';
+import { logger } from '@src/utils/logger';
 
 @Injectable()
 export class WhatsAppGateway implements WhatsAppGatewayInterface {
-    async sendMessage(to: string, message: string): Promise<any> {
-        const url = `https://graph.facebook.com/v19.0/${Env.PHONE_NUMBER_ID}/messages`;
-        const response = await axios.post(
-            url,
-            {
-                messaging_product: 'whatsapp',
-                to,
-                type: 'text',
-                text: { body: message },
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${Env.ACCESS_TOKEN}`,
-                    'Content-Type': 'application/json',
-                },
-            },
-        );
-        return response.data;
+  async sendMessage(to: string, message: string): Promise<any> {
+    const url = `https://graph.facebook.com/v19.0/${Env.PHONE_NUMBER_ID}/messages`;
+
+    try {
+      const response = await axios.post(
+        url,
+        {
+          messaging_product: 'whatsapp',
+          to,
+          type: 'text',
+          text: { body: message },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${Env.ACCESS_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      logger.info(`Mensagem enviada para ${to}: "${message}"`);
+      return response.data;
+    } catch (error) {
+      logger.error(`Erro ao enviar mensagem para ${to}: ${error.message}`);
+      throw error;
     }
-};
+  }
+}
---
+++ chatbot-api-application-proposal/src/utils/logger.ts
@@ -0,0 +1,15 @@
+import * as winston from 'winston';
+
+const { combine, timestamp, json } = winston.format;
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/app.log' }),
+  ],
+});
